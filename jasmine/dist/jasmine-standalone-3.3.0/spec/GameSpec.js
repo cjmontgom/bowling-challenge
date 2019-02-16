6a2f6d031ab78bdf7a8e842025f3184d1fdef367
Chloe <chloemontgomery01@gmail.com>
2019-02-16 12:16:20 +0000

    Game logic finished.

--- jasmine/dist/jasmine-standalone-3.3.0/spec/GameSpec.js
+++ jasmine/dist/jasmine-standalone-3.3.0/spec/GameSpec.js
@@ -6,4 +6,27 @@ describe ('Game', function() {
     game = new Game();
   });
 
+  describe ('defaults', function() {
+    it ('stores frames', function () {
+      expect(game.frames).toEqual([]);
+    });
+    it ('has a running total', function() {
+      expect(game.runningTotal).toEqual(0);
+    });
+  });
+
+  describe ('save', function() {
+    it ('adds a frame to this.frames', function() {
+      game.save('frame');
+      expect(game.frames).toContain('frame');
+    });
+  });
+
+  describe ('addToTotal', function() {
+    it ('adds to this.runningTotal', function() {
+      game.addToTotal(5)
+      expect(game.runningTotal).toEqual(5)
+    });
+  });
+
 });
--- jasmine/dist/jasmine-standalone-3.3.0/src/Game.js
+++ jasmine/dist/jasmine-standalone-3.3.0/src/Game.js
@@ -1,3 +1,12 @@
 function Game() {
+  this.frames = [];
+  this.runningTotal = 0;
+};
 
+Game.prototype.save = function(frame) {
+  this.frames.push(frame)
+};
+
+Game.prototype.addToTotal = function(num) {
+  this.runningTotal += num
 };
