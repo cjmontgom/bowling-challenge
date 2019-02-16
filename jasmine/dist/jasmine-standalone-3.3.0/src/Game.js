6dea05f5932458751c53b9e33523f66c8edaab07
Chloe <chloemontgomery01@gmail.com>
2019-02-16 12:05:14 +0000

    Starting again with different logic approach.

--- jasmine/dist/jasmine-standalone-3.3.0/src/Game.js
+++ jasmine/dist/jasmine-standalone-3.3.0/src/Game.js
@@ -1,17 +1,3 @@
-// function BowlingScores() {
-//   this.frames = [{a: 0, b: 0}, {a: 0, b: 0}, {a: 0, b: 0}, {a: 0, b: 0}, {a: 0, b: 0}, {a: 0, b: 0}, {a: 0, b: 0}, {a: 0, b: 0}, {a: 0, b: 0}, {a: 0, b: 0, c: 0}];
-//   this.pending = false
-// };
-//
-// BowlingScores.prototype._calculate = function(frame) {
-//
-//   if(frame['a'] + frame['b'] === 10) {
-//     this._switchPendingStatus()
-//   } else {
-//     return frame['a'] + frame['b']
-//   };
-// };
-//
-// BowlingScores.prototype._switchPendingStatus = function() {
-//   this.pending ? this.pending = false : this.pending = true;
-// };
+function Game() {
+
+};
